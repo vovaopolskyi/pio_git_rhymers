f9c18ba77e8629ed605f234d8736639506a61d28
Vova Opolskyi <vovaoplolski@gmail.com>
2026-03-09 15:28:10 +0100

    2. poprawa formatowania kodu ródowego w klasie HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,17 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+    private int totalRejected = 0;
 
     public int reportRejected() {
-        return totalRejected;
-    }
+    return totalRejected;
+}
 
     public void countIn(int in) {
-    if (!callCheck() && in > peekaboo())
+        if (!callCheck() && in > peekaboo()) {
             totalRejected++;
-            else
-                super.countIn(in);
+        } else {
+            super.countIn(in);
+        }
     }
 }
