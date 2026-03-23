40b996a16a2316781687e6ca5a10b6e784c13371
vovaopolskyi <vovaoplolski@gmail.com>
2026-03-23 15:28:11 +0100

    Corrected tabulation problems in file FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -3,19 +3,22 @@ package edu.kis.vh.nursery;
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
-    
+
     @Override
     public int countOut() {
-        while (!isEmpty())
-            
-        temp.countIn(super.countOut());
-        
-        int ret = temp.countOut();
-        
-        while (!temp.isEmpty())
-            
-        countIn(temp.countOut());
-        
+        int ret = -1;
+        while (!isEmpty()) {
+
+            temp.countIn(super.countOut());
+
+            ret = temp.countOut();
+
+            while (!temp.isEmpty()) {
+                countIn(temp.countOut());
+            }
+
+
+        }
         return ret;
     }
 }
